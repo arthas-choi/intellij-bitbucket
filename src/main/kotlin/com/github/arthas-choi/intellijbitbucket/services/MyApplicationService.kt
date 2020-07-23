f217dd5e9d41a985ead65e98a8b901f424a6d5eb
GitHub Action <action@github.com>
2020-07-23 16:17:46 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/arthas-choi/intellijbitbucket/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.arthas-choi.intellijbitbucket.services
+
+import com.github.arthas-choi.intellijbitbucket.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
